cc6afa1ab0f8022499f430091708f806315da7bc
Finnadip <42492360+Finnadip@users.noreply.github.com>
2023-11-22 22:09:44 +0000

    Add email property to patron
    Added to constructor
    Added getters and setters for email

--- src/bcu/cmp5332/librarysystem/model/Patron.java
+++ src/bcu/cmp5332/librarysystem/model/Patron.java
@@ -6,60 +6,70 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Patron {
-    
-    private int id;
-    private String name;
-    private String phone;
-    private final List<Book> books = new ArrayList<>();
-    
-    // TODO: implement constructor here
-    public Patron(int id, String name, String phone) {
-    	this.id = id;
-    	this.name = name;
-    	this.phone = phone;
-    }
-    
-    
-    public int getId() {
-        return id;
-    } 
-    
-    public String getName() {
-    	return name;
-    }
-    
-    public String getPhone() {
-    	return phone;
-    }
-    
-    public void setName() {
-    	this.name = name;
-    }
- 
-    public void setPhone() {
-    	this.phone = phone;
-    }
-    
-    public void setID() {
-    	this.id = id;
-    }
-    
-    public void borrowBook(Book book, LocalDate dueDate) throws LibraryException {
-        // TODO: implementation here
-    }
 
-    public void renewBook(Book book, LocalDate dueDate) throws LibraryException {
-        // TODO: implementation here
-    }
+	private int id;
+	private String name;
+	private String phone;
+	private final List<Book> books = new ArrayList<>();
+	private String email;
 
-    public void returnBook(Book book) throws LibraryException {
-        // TODO: implementation here
-    }
-    
-    public void addBook(Book book) {
-        // TODO: implementation here
-    }
+	// TODO: implement constructor here
+	public Patron(int id, String name, String phone, String email ) {
+		this.id = id;
+		this.name = name;
+		this.phone = phone;
+		this.email = email;
+	}
 
-    
+	public int getId() {
+		return id;
+	} 
+
+	public String getName() {
+		return name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setName() {
+		this.name = name;
+	}
+
+	public void setPhone() {
+		this.phone = phone;
+	}
+
+	public void setID() {
+		this.id = id;
+	}
+
+	public void setEmail() {
+		this.email = email;
+	}
+
+	public void borrowBook(Book book, LocalDate dueDate) throws LibraryException {
+		// TODO: implementation here
+		books.add(book);
+
+	}
+
+	public void renewBook(Book book, LocalDate dueDate) throws LibraryException {
+		// TODO: implementation here
+	}
+
+	public void returnBook(Book book) throws LibraryException {
+		// TODO: implementation here
+	}
+
+	public void addBook(Book book) {
+		// TODO: implementation here
+	}
+
+
 }
- 
