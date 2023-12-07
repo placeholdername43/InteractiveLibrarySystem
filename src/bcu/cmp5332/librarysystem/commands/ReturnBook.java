99632d5f456af81ac7f30de8c39139f1f0457a71
Finnadip <42492360+Finnadip@users.noreply.github.com>
2023-12-07 00:42:54 +0000

    Create ReturnBook.java

---
+++ src/bcu/cmp5332/librarysystem/commands/ReturnBook.java
@@ -0,0 +1,33 @@
+package bcu.cmp5332.librarysystem.commands;
+
+import java.time.LocalDate;
+
+import bcu.cmp5332.librarysystem.main.LibraryException;
+import bcu.cmp5332.librarysystem.model.Book;
+import bcu.cmp5332.librarysystem.model.Library;
+import bcu.cmp5332.librarysystem.model.Patron;
+
+public class ReturnBook implements Command {
+	private int bookID;
+	private int patronID;
+
+	public ReturnBook(int bookID, int patronID) {
+		this.bookID = bookID;
+		this.patronID = patronID;
+	}
+
+
+	@Override
+	public void execute(Library library, LocalDate currentDate) throws LibraryException{
+		Patron patron = library.getPatronByID(patronID);
+		Book book = library.getBookByID(bookID);
+	
+		patron.returnBook(book);
+		System.out.println("Patron " + patronID + " returned " + "Book " + bookID);
+	
+	}
+	
+
+
+
+}
