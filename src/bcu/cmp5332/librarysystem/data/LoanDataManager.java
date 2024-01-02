7e15576fd623271cad3c0e7785931deccb5f5836
Finnadip <42492360+Finnadip@users.noreply.github.com>
2024-01-02 23:54:47 +0000

    add check to see if loan is null or not
    this fixes the problem with attempting to store nothing

--- src/bcu/cmp5332/librarysystem/data/LoanDataManager.java
+++ src/bcu/cmp5332/librarysystem/data/LoanDataManager.java
@@ -14,54 +14,55 @@ import java.time.LocalDate;
 import java.util.Scanner;
 
 public class LoanDataManager implements DataManager {
-    
-    public final String RESOURCE = "./resources/data/loans.txt";
 
-    @Override
-    public void loadData(Library library) throws IOException, LibraryException {
-    	try (Scanner sc = new Scanner(new File(RESOURCE))) {
-        	int line_idx = 1;
-        	while(sc.hasNextLine()) {
-        		String line = sc.nextLine();
-        		String[] properties = line.split(SEPARATOR, -1);
-        		
-        		try {
-        			int patronID = Integer.parseInt(properties[0]);
-        			int bookID = Integer.parseInt(properties[1]);
-        			LocalDate startDate = LocalDate.parse(properties[2]);
-        			LocalDate dueDate = LocalDate.parse(properties[3]);
-        			
-        			
-        			Patron patron = library.getPatronByID(patronID);
-        			Book book = library.getBookByID(bookID);
-        			
-        			Loan loan = new Loan(patron, book, startDate, dueDate);
-        			
-        			book.setLoan(loan);
-        			patron.addBook(book);
-        			
-        			
-        		}catch (NumberFormatException ex) {
-                    throw new LibraryException("Unable to parse loan id " + properties[0] + " on line " + line_idx
-                            + "\nError: " + ex);
-        		}
-        	}
-        }
-    }
+	public final String RESOURCE = "./resources/data/loans.txt";
 
-    @Override
-    public void storeData(Library library) throws IOException {
-        try (PrintWriter out = new PrintWriter(new FileWriter(RESOURCE))){
-        	for(Book book : library.getBooks()){
-        		Loan loan = book.getLoan();
-        		out.print(loan.getPatron().getId() + SEPARATOR);
-                out.print(loan.getBook().getId() + SEPARATOR);
-                out.print(loan.getStartDate() + SEPARATOR);
-                out.print(loan.getDueDate() + SEPARATOR);
-                out.println();
-        	}
-        }
-    }
-    
+	@Override
+	public void loadData(Library library) throws IOException, LibraryException {
+		try (Scanner sc = new Scanner(new File(RESOURCE))) {
+			int line_idx = 1;
+			while(sc.hasNextLine()) {
+				String line = sc.nextLine();
+				String[] properties = line.split(SEPARATOR, -1);
+
+				try {
+					int patronID = Integer.parseInt(properties[0]);
+					int bookID = Integer.parseInt(properties[1]);
+					LocalDate startDate = LocalDate.parse(properties[2]);
+					LocalDate dueDate = LocalDate.parse(properties[3]);
+
+
+					Patron patron = library.getPatronByID(patronID);
+					Book book = library.getBookByID(bookID);
+
+					Loan loan = new Loan(patron, book, startDate, dueDate);
+
+					book.setLoan(loan);
+					patron.addBook(book);
+
+
+				}catch (NumberFormatException ex) {
+					throw new LibraryException("Unable to parse loan id " + properties[0] + " on line " + line_idx
+							+ "\nError: " + ex);
+				}
+			}
+		}
+	}
+
+	@Override
+	public void storeData(Library library) throws IOException {
+		try (PrintWriter out = new PrintWriter(new FileWriter(RESOURCE))){
+			for(Book book : library.getBooks()){
+				Loan loan = book.getLoan();
+				if(loan != null) {
+					out.print(loan.getPatron().getId() + SEPARATOR);
+					out.print(loan.getBook().getId() + SEPARATOR);
+					out.print(loan.getStartDate() + SEPARATOR);
+					out.print(loan.getDueDate() + SEPARATOR);
+					out.println();
+				}
+			}
+		}
+	}
+
 }
- 
