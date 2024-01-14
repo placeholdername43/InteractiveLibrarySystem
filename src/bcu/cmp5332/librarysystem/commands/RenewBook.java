437e07bc24cf0b6ed64c5509b4d7f9fb7c5eec5a
Finnadip <42492360+Finnadip@users.noreply.github.com>
2024-01-14 17:26:18 +0000

    impelement renewbook

--- src/bcu/cmp5332/librarysystem/commands/RenewBook.java
+++ src/bcu/cmp5332/librarysystem/commands/RenewBook.java
@@ -3,14 +3,33 @@ package bcu.cmp5332.librarysystem.commands;
 import java.time.LocalDate;
 
 import bcu.cmp5332.librarysystem.main.LibraryException;
+import bcu.cmp5332.librarysystem.model.Book;
 import bcu.cmp5332.librarysystem.model.Library;
+import bcu.cmp5332.librarysystem.model.Patron;
 
 public class RenewBook implements Command{
+	
+	private final int patronID;
+	private final int bookID;
+
+	public RenewBook(int patronID, int bookID) {
+		this.patronID = patronID;
+		this.bookID = bookID;
+	}
 
 	@Override
 	public void execute(Library library, LocalDate currentDate) throws LibraryException {
-		// TODO Auto-generated method stub
+		LocalDate dueDate =  currentDate.plusDays(library.getLoanPeriod());
+		Patron patron = library.getPatronByID(patronID);
+		Book book = library.getBookByID(bookID);
 		
+		if(patron.getIsDeleted()) {
+			throw new LibraryException("No patron with this ID");
+		}else if (book.getIsDeleted()) {
+			throw new LibraryException("No book with this ID");
+		}
+		
+		patron.renewBook(book, dueDate);
 	}
 
 }
